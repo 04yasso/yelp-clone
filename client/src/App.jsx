aab8c2d11fb48316c588e5698eed39f88915b698
Yaseen <yaz@berkeley.edu>
2024-03-15 18:58:03 -0700

    Add client folder

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import UpdatePage from "./routes/UpdatePage";
+import RestaurantDetailPage from "./routes/RestaurantDetailPage";
+import { RestaurantsContextProvider } from "./context/RestaurantsContext";
+
+const App = () => {
+  return (
+    <RestaurantsContextProvider>
+      <div className="container">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/restaurants/:id/update" element={<UpdatePage />} />
+            <Route path="/restaurants/:id" element={<RestaurantDetailPage />} />
+          </Routes>
+        </Router>
+      </div>
+    </RestaurantsContextProvider>
+  );
+};
+
+export default App;
